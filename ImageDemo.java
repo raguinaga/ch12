f2b3734648de4ad895bb22dfda39e0736e071e2b
Roberto Aguinaga <raguinaga1@outlook.com>
2020-06-01 18:50:01 -0500

    Fixed location specifier in image source

--- ImageDemo.java
+++ ImageDemo.java
@@ -1,11 +1,11 @@
 package firstGui;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.application.Application; // Abstract class for a GUI app
+import javafx.stage.Stage;             // Window object
+import javafx.scene.Scene;             // Stuff to go in the window
+import javafx.scene.layout.HBox;       // Horizontal container for the scene
+import javafx.scene.image.Image;       // Image object
+import javafx.scene.image.ImageView;   // Graphical representation of te image
 
 /**
  * An image demo
@@ -14,9 +14,9 @@ public class ImageDemo extends Application {
 
     @Override
     public void start(Stage stage) {
-        
+
         // Create an image object
-        Image image = new Image("file:HotAirBalloon.png");
+        Image image = new Image("file:D:\\java\\twelveRedux\\images\\HotAirBalloon.jpg");
 
         // Create an image view object
         ImageView imageView = new ImageView(image);
